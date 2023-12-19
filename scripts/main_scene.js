d9c1299ed1d535aa3e429e788514542921b99ecc
renka <r.oomori.sys22@morijyobi.ac.jp>
2023-12-19 14:17:24 +0900

    taroとhanakoの表示

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -6,11 +6,18 @@ class MyScene extends Phaser.Scene {
     preload() {
         // 画像の読み込み(使用する時の名前, パス)
        this.load.image('back', 'assets/background.png');
+       this.load.image('taro', 'assets/taro.png');
+       this.load.image('hanako', 'assets/hanako.png');
    }
 
    // シーン初期化処理
    create() {
        this.add.image(400, 225, 'back');
+       const taro = this.physics.add.sprite(50, 50, 'taro');
+       const hanako = this.physics.add.sprite(750, 400, 'hanako');
+       this.taro = taro;
+       this.hanako = hanako;
+
    }
    
     update() {
